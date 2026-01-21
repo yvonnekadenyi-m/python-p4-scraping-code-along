e9cdc4a38d1b4c6b3eaedddf4b9fec09d2ca1eff
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-21 14:16:55 +0300

    added to files

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,11 @@
 class Course:
-    pass
+    def __init__(self, title=None, schedule=None, description=None):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self):
+        output = ''
+        output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '------------------'
+        return output
--- lib/Scraper.py
+++ lib/Scraper.py
@@ -5,4 +5,27 @@ import ipdb
 
 
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self):
+        doc = BeautifulSoup(requests.get(
+            "http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
+        return doc
+
+    def get_courses(self):
+        return self.get_page().select('.post')
+
+    def make_courses(self):
+        for course in self.get_courses():
+            title = course.select("h2")[0].text if course.select("h2") else ''
+            date = course.select(".date")[0].text if course.select(".date") else ''
+            description = course.select("p")[0].text if course.select("p") else ''
+
+            new_course = Course(title=title, schedule=date, description=description)
+            self.courses.append(new_course)
+        return self.courses
+
+    def print_courses(self):
+        for course in self.make_courses():
+            print(course)
